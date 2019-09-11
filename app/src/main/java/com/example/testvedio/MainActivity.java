08d6f9a17f0ec46d139e3598d0aac30c5f47b9d7
peili2017 <3291212003@qq.com>
2019-09-11 08:19:07 +0800

    ccccc

--- app/src/main/java/com/example/testvedio/MainActivity.java
+++ app/src/main/java/com/example/testvedio/MainActivity.java
@@ -4,8 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends Activity {
 
@@ -24,5 +33,48 @@ public class MainActivity extends Activity {
         /*Uri uri = Uri.parse("https://blog.csdn.net/Leslie_LN/article/details/91584144");
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
         startActivity(intent);*/
+
+        /*File outputImage = new File(Environment.getExternalStorageDirectory(),
+                "tempImage" + ".jpg");
+        try {
+            if (outputImage.exists()) {
+                outputImage.delete();
+            }
+            outputImage.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Uri imageUri = Uri.fromFile(outputImage);
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, 1);*/
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        Toast.makeText(MainActivity.this,keyCode+"++",Toast.LENGTH_SHORT).show();
+        if(keyCode ==   KeyEvent.KEYCODE_BACK)
+        {
+            return   true;
+        }
+        if(keyCode ==   KeyEvent.KEYCODE_VOLUME_DOWN)
+        {
+            return   true;
+        }
+        if(keyCode ==   KeyEvent.KEYCODE_VOLUME_UP)
+        {
+            return   true;
+        }
+        if(keyCode ==   KeyEvent.KEYCODE_SEARCH)
+        {
+            return   true;
+        }
+        if(keyCode ==   KeyEvent.KEYCODE_MENU)
+        {
+            return   true;
+        }
+        return super.onKeyDown(keyCode,   event);
     }
 }
